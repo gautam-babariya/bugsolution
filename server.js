372892c0b392c3e68b05b03533b00688018b7e67
gautam <gautambabariya0125@gmail.com>
2024-05-28 20:41:17 +0530

    bhai work thy ja

--- server.js
+++ server.js
@@ -1,36 +1,53 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const multer = require('multer');
+var { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
-const cors = require('cors');
+const fs = require('fs');
 const path = require('path');
 
 
+const app = express();
 
 // Cloudinary configuration
-
 cloudinary.config({
     cloud_name: 'delde3vvw',
     api_key: '677662562595255',
     api_secret: 'OtKmdP9jhhYIXObdsuUmVbDCuV4'
 });
 
-// Create a new Express application
-const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-// Enable CORS
-app.use(cors());
+app.post('/api/upload', upload.single('video'), (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+  
+    const stream = Readable.from(req.file.buffer);
+    
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { resource_type: 'video' },
+      (error, result) => {
+        if (error) {
+          return res.status(500).json({ error: 'Failed to upload to Cloudinary' });
+        }
+  
+        res.status(200).json({
+          message: 'File uploaded successfully',
+          fileUrl: result.secure_url
+        });
+      }
+    );
+  
+    stream.pipe(uploadStream);
+  });
 
-// Parse JSON requests
-app.use(express.json());
-
-// const parser = multer({ storage: storage });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
